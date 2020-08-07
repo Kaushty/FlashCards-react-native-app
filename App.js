59867a2e320426c17b6f4ab7b528a079c9657e90
kaushty <kaushikshetty4898@gmail.com>
2020-08-07 15:13:49 +0530

    fix: helper functions fixed to store data properly.
    Now Helper functions store data directly to the AsyncStorage.

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import Constants from 'expo-constants'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import DeckList from './components/DeckListView'
+import AddDeck from './components/AddDeck'
 
 function AppStatusBar () {
   return(
@@ -16,11 +20,20 @@ function AppStatusBar () {
   )
 }
 
+const Tab = createBottomTabNavigator()
+
 export default function App() {
   return (
-    <View style={{flex:1}}>
-      <AppStatusBar />
-      <DeckList />
-    </View>
-  );
+   <NavigationContainer>
+      <View style={{flex:1}}>
+        <AppStatusBar />
+        <Tab.Navigator>
+          <Tab.Screen name='Decks' component={ DeckList } />
+          <Tab.Screen name='Add Deck' component={ AddDeck } />
+        </Tab.Navigator>
+        {/* <DeckList />
+        <AddDeck /> */}
+      </View>
+   </NavigationContainer>
+  )
 }
--- utils/helper.js
+++ utils/helper.js
@@ -1,9 +1,9 @@
 import { AsyncStorage } from 'react-native'
 // Store the data in asyncStorage
 
-export const STORAGE_KEY = 'UDACICARDS'
+export const STORAGE_KEY = 'UDACICARDS:Decks'
 
-const data = {
+const startData = {
     React: {
       title: 'React',
       questions: [
@@ -28,27 +28,41 @@ const data = {
     }
   }
 
-// Define methods to access the data.
+// Define methods to access the data
 export const _store_data = async () => {
     await AsyncStorage.setItem(
         STORAGE_KEY,
-        JSON.stringify({
-            cards: data,
-        })
+        JSON.stringify(startData),
     ).catch((error) => {
         console.error('Error while setting data to AsynStorage',error)
     })
 }
-
 
 // Method to fetch all the cards to map over
 export const getDecks = async () => {
     try{
         const response = await AsyncStorage.getItem(STORAGE_KEY)        
         const data = JSON.parse(response)
-        return data.cards
+        let newData = {}
+        if(data === null){
+            await _store_data()  
+            newData = await getDecks()
+            return newData
+        }
+        console.log(JSON.stringify(data))
+        return data
     }catch(error){
         console.log('Error while fetching data from AsynStorage', error)
+    }
+}
+
+// Function to clear AsyncStorage 
+export const clearDecks = async () => {
+    try{
+        await AsyncStorage.removeItem(STORAGE_KEY)        
+    }
+    catch(error){
+        console.log('Failed to clear AsyncStorage', error)
     }
 }
 
@@ -57,7 +71,7 @@ export const getDeck = async (deckId) => {
     try{
         const response = await AsyncStorage.getItem(STORAGE_KEY)        
         const data = JSON.parse(response)
-        return data[deckId]
+        return data[deckId] ? data[deckId] : {}
     }catch(error){
         console.error('Error while fetching data from AsynStorage', error)
     }
@@ -73,12 +87,12 @@ export const saveDeckTitle = async (deckTitle) => {
         await AsyncStorage.mergeItem(
             STORAGE_KEY,
             JSON.stringify({
-                deckTitle: newDeck,
+                [deckTitle]: newDeck,
             })
         )
     }
     catch(error){
-        console.error('Error while saving deck title to AsynStorage', error)
+        console.error('Error while saving deck to AsynStorage', error)
     }
 }
 
@@ -101,3 +115,5 @@ export const saveQuestion = async (deckId, card) => {
         console.error('Error occured while saving question to AsyncStorage ', error)
     }
 }
+
+// Function to delete a deck
